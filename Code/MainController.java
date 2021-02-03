b92b28bf9434d6ab153c82e59747270aa09cfa3a
nichlas0010 <nichlas0010@gmail.com>
2021-02-03 17:08:08 +0000

    Adds the entrypoint and the class for the main controller

---
+++ Code/MainController.java
@@ -0,0 +1,22 @@
+
+
+public class MainController {
+
+    /**
+     * Entry method. Creates a MainController. All of the actual creation should be handled in the {@link #MainController() Constructor} method.
+     *
+     * @param args TODO
+     */
+    public static void main(String[] args) {
+        new MainController();
+    }
+
+    /**
+     * Constructor for the MainController. Creates all necessary GUIElements and will be the nervous system of the programme
+     */
+    public MainController() {
+        //TODO
+        return
+    }
+
+}
