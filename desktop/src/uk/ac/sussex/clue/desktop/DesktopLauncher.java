22ea7f90ba1a4975068aca37d4aa021aed76b1b4
nichlas0010 <nichlas0010@gmail.com>
2021-02-08 12:53:47 +0000

    adds libgdx

---
+++ desktop/src/uk/ac/sussex/clue/desktop/DesktopLauncher.java
@@ -0,0 +1,22 @@
+package uk.ac.sussex.clue.desktop;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import uk.ac.sussex.clue.MainController;
+
+import java.awt.*;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Dimension dimension = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.fullscreen = true;
+		config.height = (int) dimension.getHeight();
+		config.width = (int) dimension.getWidth();
+		config.width = 1920;
+		config.title = "Clue!";
+		new LwjglApplication(new MainController(), config);
+	}
+}
