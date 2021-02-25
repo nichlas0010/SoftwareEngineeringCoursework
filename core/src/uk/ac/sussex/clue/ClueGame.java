8af4501053ca8492ba1932e38e61c3ca15769fe6
nichlas0010 <nichlas0010@gmail.com>
2021-02-25 16:16:05 +0000

    adds the cluegame class, no actual logic implemented

---
+++ core/src/uk/ac/sussex/clue/ClueGame.java
@@ -0,0 +1,8 @@
+package uk.ac.sussex.clue;
+
+public class ClueGame extends Window {
+    public ClueGame(MainController mc) {
+        super(mc);
+
+    }
+}
--- core/src/uk/ac/sussex/clue/NewGame.java
+++ core/src/uk/ac/sussex/clue/NewGame.java
@@ -3,5 +3,7 @@ package uk.ac.sussex.clue;
 public class NewGame extends Window {
     public NewGame(MainController mc) {
         super(mc);
+        // TODO: New game menu
+        mc.setScreen(new ClueGame(mc));
     }
 }
